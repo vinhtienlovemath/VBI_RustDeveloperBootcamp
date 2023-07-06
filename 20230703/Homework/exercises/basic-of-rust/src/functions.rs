d875d3d89f92db523241b10b55c58a03ee557df1
vinhtienlovemath <vinhtienlovemath@gmail.com>
2023-07-06 17:01:35 +0700

    Pass ./20230703/Homework/exercises/basic-of-rust/src/functions.rs

--- 20230703/Homework/exercises/basic-of-rust/src/functions.rs
+++ 20230703/Homework/exercises/basic-of-rust/src/functions.rs
@@ -1,7 +1,7 @@
 // Exercise 1
 // Fix all errors 
-fn sum(x, y: i32) {
-    x + y;
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
 }
 
 //Exercise 2
@@ -11,7 +11,11 @@ fn sum(x, y: i32) {
 pub fn sum_one_to_n(n: u32) -> u32 {
     // your code for summing all digits from 1 to `n` (inclusive) should go
     // here (you can remove the sample return of `0`)
-    0
+    let mut sum = 0;
+    for i in 1..n+1 {
+        sum = sum + i;
+    }
+    sum
 }
 
 // Exercise 3
@@ -19,13 +23,29 @@ pub fn sum_one_to_n(n: u32) -> u32 {
 // Problem: Calculate the average of a list of numbers
 // Output: Average Number 
 fn calculate_average(numbers: &[f64]) -> f64 {
-    todo!()
+    if numbers.len() == 0 {
+        0.0
+    }
+    else {
+        let mut sum = 0.0;
+        for number in numbers {
+            sum = sum + number;
+        }
+        let size = numbers.len() as f64;
+        sum/size
+    }
 }
 
 // Exercise 4
 // Calculate the sum of all even numbers in a list
 fn sum_even_numbers(numbers: &[i32]) -> i32 {
-    todo!()
+    let mut even_sum = 0;
+    for i in numbers {
+        if i % 2 == 0 {
+            even_sum = even_sum + i;
+        }
+    }
+    even_sum
 }
 
 
