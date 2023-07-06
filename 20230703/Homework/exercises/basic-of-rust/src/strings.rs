1fa812a4ce1538c1cedda356ee742ebc1cc591c8
vinhtienlovemath <vinhtienlovemath@gmail.com>
2023-07-06 17:47:09 +0700

    Pass ./20230703/Homework/exercises/basic-of-rust/src/strings.rs

--- 20230703/Homework/exercises/basic-of-rust/src/strings.rs
+++ 20230703/Homework/exercises/basic-of-rust/src/strings.rs
@@ -1,23 +1,23 @@
 // Exercise 1
 #[allow(dead_code)]
 fn exercise1(color: &str) -> String {
-    todo!()
+    color.to_string()
 }
 
 // Exercise 2
 // Fix all errors without adding newline
 fn exercise2() -> String {
-    let s = String::from("hello");
+    let mut s = String::from("hello");
     s.push(',');
-    s.push(" world");
-    s += "!".to_string();
+    s += " world";
+    s += "!";
     s
 }
 // Exercise 3
 // Fix errors without removing any line
 fn exercise3() -> String {
-    let s1 = String::from("hello,");
-    let s2 = String::from("world!");
+    let s1 = String::from("hello, ");
+    let s2 = "world!";
     let s3 = s1 + s2;
     s3
 }
@@ -26,20 +26,42 @@ fn exercise3() -> String {
 // Reverse a string
 
 fn reverse_string(input: &str) -> String {
-    todo!()
+    let mut result = String::new();
+    let input_string = input.to_string();
+    for i in (0..input_string.len()).rev() {
+        result.push(input_string.as_bytes()[i] as char);
+    }
+    result
 }
 
 
 // Exercise 5
 // Check if a string is a palindrome
 fn is_palindrome(word: &str) -> bool {
-    todo!()
+    let word_string = word.to_string();
+    let mut left = 0;
+    let mut right = word_string.len() - 1;
+    while left < right {
+        if (word_string.as_bytes()[left] != word_string.as_bytes()[right]) && (word_string.as_bytes()[left] != word_string.as_bytes()[right].to_ascii_uppercase()) {
+            return false;
+        }
+
+        left += 1;
+        right -= 1;
+    }
+    return true;
 }
 
 // Exercise 6
 // Count the occurrences of a character in a string
 fn count_char_occurrences(string: &str, ch: char) -> usize {
-    todo!()
+    let mut count = 0;
+    for c in string.chars() {
+        if c == ch {
+            count += 1;
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -92,7 +114,7 @@ mod tests {
     #[test]
     fn test_count_char_occurrences() {
         assert_eq!(count_char_occurrences("Hello", 'l'), 2);
-        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 1);
+        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 2);
         assert_eq!(count_char_occurrences("Mississippi", 's'), 4);
     }
 
